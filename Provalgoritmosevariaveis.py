41f1b57d4145a1e4f593fcd1dbb779be9e25bc1e
ysltwokk <lorranmesquita071@gmail.com>
2024-11-15 06:00:06 -0300

    Add files via upload

---
+++ Provalgoritmosevariaveis.py
@@ -0,0 +1,10 @@
+#solicitar base e área do retangulo
+base = float(input("Digite a base do retnângulo:"))
+altura = float(input('Digite a área do retângulo:'))
+
+#caucular a altura x a base
+área = base * altura 
+print(base * altura)
+print(f"A área do retângulo é: {área}")
+
+
